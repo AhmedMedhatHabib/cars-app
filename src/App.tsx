dec07353d2d7682b1eaf7a16507edc2a86ca1cd8
Med <ahmed.medhat.habib@gmail.com>
2023-09-23 00:50:32 +0300

    test initial api mock

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
-import './App.css';
 import { useGetUserByIdQuery } from './service/queries/users';
 
 function App() {
@@ -9,8 +8,10 @@ function App() {
 
   const { data, error, isLoading } = useGetUserByIdQuery(1);
   console.log(data, error, isLoading);
+
   return (
-    <>
+    <div className="w-full">
+      <h1 className="text-3xl font-bold underline">title</h1>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -31,7 +32,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </div>
   );
 }
 
