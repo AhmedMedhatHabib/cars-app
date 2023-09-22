28c241b5767b87f06b695e40cc9443c73bb0eac2
Med <ahmed.medhat.habib@gmail.com>
2023-09-22 23:44:30 +0300

    Add `getUserUsageByUserId` query

--- src/service/queries/users.ts
+++ src/service/queries/users.ts
@@ -1,6 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import type { User } from '../types';
 import config from '../../config';
+import { UserUsage } from '../types/user';
 
 export const userApi = createApi({
   reducerPath: 'users',
@@ -9,7 +10,10 @@ export const userApi = createApi({
     getUserById: builder.query<User, number>({
       query: id => `/users/${id}`,
     }),
+    getUserUsageByUserId: builder.query<UserUsage, number>({
+      query: id => `/users/${id}/usage`,
+    }),
   }),
 });
 
-export const { useGetUserByIdQuery } = userApi;
+export const { useGetUserByIdQuery, useGetUserUsageByUserIdQuery } = userApi;
--- src/service/types/user.ts
+++ src/service/types/user.ts
@@ -3,3 +3,16 @@ export type User = {
   name: string;
   img: string;
 };
+
+export type Usage = {
+  energy: string;
+  range: string;
+  break_fluid: string;
+  tire_wear: string;
+};
+
+export type UserUsage = {
+  id: number;
+  userId: number;
+  usage: Usage;
+};
