f36d238467d8b552cb772e7a8feec8c71fae2955
Med <ahmed.medhat.habib@gmail.com>
2023-09-23 00:50:47 +0300

    add translation config

---
+++ src/localization/index.tsx
@@ -0,0 +1,26 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+
+import arTranslation from './ar.json';
+import enTranslation from './en.json';
+
+i18n.use(initReactI18next).init({
+  debug: true,
+  resources: {
+    ar: { translation: arTranslation },
+    en: { translation: enTranslation },
+  },
+  lng: 'en',
+  fallbackLng: 'en',
+
+  interpolation: {
+    escapeValue: false,
+  },
+});
+
+i18n.on('languageChanged', lng => {
+  document.documentElement.lang = lng;
+  document.documentElement.dir = lng === 'ar' ? 'rtl' : 'ltr';
+});
+
+export default i18n;
