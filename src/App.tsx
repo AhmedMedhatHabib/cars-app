af95bc60809945f0eb7c90dd3ce40a82ce56c01e
Med <ahmed.medhat.habib@gmail.com>
2023-09-22 23:03:52 +0300

    Use Cars API

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { useGetCarsQuery } from './service/queries/cars'
 
 function App() {
   const [count, setCount] = useState(0)
-
+  
+  const { data, error, isLoading } = useGetCarsQuery("")
+  console.log(data, error, isLoading);
   return (
     <>
       <div>
